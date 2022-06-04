ecfe12942f7692ddf9d094b747fbce374d365e7e
Sakibul Islam <sakibulislam25800@gmail.com>
2022-06-04 14:32:13 +0100

    Update printEpisode.js

--- src/utils/printEpisode.js
+++ src/utils/printEpisode.js
@@ -1,14 +1,15 @@
+const { gray } = require("chalk");
 const chalk = require("chalk");
 
 const printEpisode = (episode) => {
   const { title, link, currentEpisode, totalEpisodes, status } = episode;
   const { cyan, green, yellow } = chalk; 
-  console.log(
-  `${cyan("Title: ")}: ${green(title)} 
-  ${cyan("Episodes: ")}: ${yellow(totalEpisodes)}
-  ${cyan{"Current Episode: "} : ${yellow(currentEpisode)}
-  ${cyan("Link: ")}: ${gray(link)}
-  ${cyan("Status: ")}: ${status}`
+  console.log(`
+    ${cya("Title")}: ${green(title)}
+    ${cyan("Episodes")}: ${yellow(totalEpisodes)}
+    ${cyan("Current Episode")}: ${currentEpisode}
+    ${cyan("Link")}: ${gray(link)}
+    ${cyan(status)}: ${status}`
   )
 };
 
